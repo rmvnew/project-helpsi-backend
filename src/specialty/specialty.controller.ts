19d59460b0285920384f8e2000182d2e4ccd97db
Ricardo <rmvnew@gmail.com>
2023-09-06 21:37:26 -0400

    chore: add crud specialty

--- src/specialty/specialty.controller.ts
+++ src/specialty/specialty.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SpecialtyService } from './specialty.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import AccessProfile from 'src/auth/enums/permission.type';
+import { PermissionGuard } from 'src/auth/shared/guards/permission.guard';
 import { CreateSpecialtyDto } from './dto/create-specialty.dto';
 import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
+import { SpecialtyService } from './specialty.service';
 
 @Controller('specialty')
+@ApiTags('Specialty')
+@ApiBearerAuth()
+
+
 export class SpecialtyController {
-  constructor(private readonly specialtyService: SpecialtyService) {}
+  constructor(private readonly specialtyService: SpecialtyService) { }
 
   @Post()
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
   create(@Body() createSpecialtyDto: CreateSpecialtyDto) {
     return this.specialtyService.create(createSpecialtyDto);
   }
 
   @Get()
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
   findAll() {
     return this.specialtyService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
   findOne(@Param('id') id: string) {
-    return this.specialtyService.findOne(+id);
+    return this.specialtyService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
   update(@Param('id') id: string, @Body() updateSpecialtyDto: UpdateSpecialtyDto) {
-    return this.specialtyService.update(+id, updateSpecialtyDto);
+    return this.specialtyService.update(id, updateSpecialtyDto);
   }
 
   @Delete(':id')
+  @UseGuards(PermissionGuard(AccessProfile.ADMIN))
   remove(@Param('id') id: string) {
-    return this.specialtyService.remove(+id);
+    return this.specialtyService.remove(id);
   }
 }
--- src/specialty/specialty.service.ts
+++ src/specialty/specialty.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateSpecialtyDto } from './dto/create-specialty.dto';
 import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
+import { Specialty } from './entities/specialty.entity';
 
 @Injectable()
 export class SpecialtyService {
-  create(createSpecialtyDto: CreateSpecialtyDto) {
-    return 'This action adds a new specialty';
+
+
+  constructor(
+    @InjectRepository(Specialty)
+    private readonly specialtyRepository: Repository<Specialty>
+  ) { }
+
+
+  async create(createSpecialtyDto: CreateSpecialtyDto) {
+
+    const { specialty_name } = createSpecialtyDto
+
+    const isRegistered = await this.findByName(specialty_name.toUpperCase())
+
+    if (isRegistered) {
+      throw new BadRequestException(`A especialidade já está cadastrada!!`)
+    }
+
+    const specialty = this.specialtyRepository.create(createSpecialtyDto)
+    specialty.specialty_name = specialty_name.toUpperCase()
+
+    return this.specialtyRepository.save(specialty)
   }
 
-  findAll() {
-    return `This action returns all specialty`;
+  async findByName(name: string) {
+    return this.specialtyRepository.findOne({
+      where: {
+        specialty_name: name
+      }
+    })
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} specialty`;
+  async findAll() {
+    return this.specialtyRepository.find()
   }
 
-  update(id: number, updateSpecialtyDto: UpdateSpecialtyDto) {
-    return `This action updates a #${id} specialty`;
+  async findOne(id: string) {
+    return this.specialtyRepository.findOne({
+      where: {
+        specialty_id: id
+      }
+    })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} specialty`;
+  async update(id: string, updateSpecialtyDto: UpdateSpecialtyDto) {
+
+    const isRegistered = await this.findOne(id)
+
+    if (!isRegistered) {
+      throw new NotFoundException(`Especialidade não foi encontrada!`)
+    }
+
+    const { specialty_name } = updateSpecialtyDto
+
+    const specialty = await this.specialtyRepository.preload({
+      specialty_id: id,
+      ...updateSpecialtyDto
+    })
+
+
+    if (specialty_name) {
+      specialty.specialty_name = specialty_name.toUpperCase()
+    }
+
+    return this.specialtyRepository.save(specialty)
+  }
+
+  async remove(id: string) {
+    const isRegistered = await this.findOne(id)
+
+    if (!isRegistered) {
+      throw new NotFoundException(`Especialidade não foi encontrada!`)
+    }
+
+    this.specialtyRepository.delete(id)
+
   }
 }
